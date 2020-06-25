511f256bdc313b76c862d862c639f22eebcf2ce9
Lasse Berglund <exmoffen@gmail.com>
2020-06-25 11:54:28 +0100

    Day 1 Part 2

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,7 +1,26 @@
-import java.lang.Integer.sum
-
 fun main() {
-    val modules = readInts("input/day1.txt")
+    val modules = readInts("input/modules.txt")
+    part1(modules)
+    part2(modules)
+}
+
+fun part1(modules: List<Int>) {
     val fuel = modules.map { (it / 3) - 2 }.sum()
     println(fuel)
+}
+
+fun part2(modules: List<Int>) {
+    val totalFuel = modules.map { requiredFuel(it) }.sum()
+    assert(requiredFuel(14) == 2)
+    assert(requiredFuel(1969) == 966)
+    assert(requiredFuel(100756) == 50346)
+    println(totalFuel)
+}
+
+fun requiredFuel(weight: Int): Int {
+    val additionalFuel = (weight / 3 - 2)
+    return if (additionalFuel <= 0)
+        0
+    else
+        additionalFuel + requiredFuel(additionalFuel)
 }
