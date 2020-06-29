9d26314f31176bb3fed09f3d1d057c97f5c0b914
Lasse Berglund <exmoffen@gmail.com>
2020-06-29 09:51:06 +0100

    Day 6 Part 2

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,31 +1,44 @@
 import java.io.File
 
-@ExperimentalStdlibApi
 fun main() {
     val exampleGraph = createGraph("input/day-6-small-map.txt")
     assert(depths(exampleGraph) == 42)
     val graph = createGraph("input/day-6-map.txt")
     println(depths(graph))
+
+    val santasOrbits = allOrbits("SAN", graph)
+    val yourOrbits = allOrbits("YOU", graph)
+    loop@for(planet in yourOrbits) {
+        if(santasOrbits.contains(planet)) {
+            println(santasOrbits.indexOf(planet) + yourOrbits.indexOf(planet))
+            break@loop
+        }
+    }
 }
 
 private fun createGraph(fileName: String): Map<String, String> {
     val graph = mutableMapOf<String, String>()
     File(fileName).readLines().map {
-        val from = it.split(")")[0]
-        val to = it.split(")")[1]
-        graph.put(to, from)
+        val parent = it.split(")")[0]
+        val child = it.split(")")[1]
+        graph.put(child, parent)
     }
     return graph
 }
 
-@ExperimentalStdlibApi
+
+private fun allOrbits(planet: String, graph: Map<String, String>): Set<String> {
+    val parent = graph.entries.find { (k, _) -> k == planet }?.value ?: return emptySet()
+    return setOf(parent) + allOrbits(parent, graph)
+}
+
 private fun depths(graph: Map<String, String>) = graph.keys.map {
-            countOrbits(it, graph, 0)
-        }.sum()
+    countOrbits(it, graph, 0)
+}.sum()
 
 private fun countOrbits(node: String, graph: Map<String, String>, acc: Int): Int {
-    val planet = graph.get(node)
-    return if(planet == null) {
+    val planet = graph[node]
+    return if (planet == null) {
         acc
     } else {
         countOrbits(planet, graph, 1 + acc)
